88a16a5338bccb6daf9387dbb16103424124116d
Piyush Mehta <me@piyushmehta.com>
2019-09-15 09:15:09 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout, Menu } from "antd";
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          style={{ lineHeight: "64px" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Menu.Item key="1">Laen Daen Home</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <div style={{ background: "#fff", padding: 24, minHeight: "90vh" }}>
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Made with{" "}
+        <span role="img" aria-label="emoji">
+          ❤️
+        </span>{" "}
+        by Team Whatever
+      </Footer>
+    </Layout>
   );
 }
 
